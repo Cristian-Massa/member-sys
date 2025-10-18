450c343843579b4499fd5e1269922f9767b88bb4
Chepy2 <anthony.gonzalez@estudiantes.utec.edu.uy>
2025-10-18 18:12:32 -0300

    puto
    el que lo lea

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,10 @@
+#include <stdio.h>
+#include "database/database.h"
+#include "finger-print/finger-print.h"
+#include "members/members.h"
+#include "payments/payments.h"
+
+
 int main(){
 
     return 0;
